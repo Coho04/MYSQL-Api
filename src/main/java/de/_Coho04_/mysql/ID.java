72ab5ad3c79a36e6c10d3d1d2aeae9be181a3f09
cohoh <cohohohn04@gmail.com>
2022-01-26 00:01:10 +0100

    ADD User to MYSQL APi

---
+++ src/main/java/de/_Coho04_/mysql/ID.java
@@ -0,0 +1,9 @@
+package de._Coho04_.mysql;
+
+public class ID {
+
+    public static String hostname = "138.201.202.55";
+    public static String user = "Basti";
+    public static String password = "basti?hibst";
+    public static Integer port = 3306;
+}
