89046269bdc8ea464f8ded69f18c18cd0b94a5c2
Collin Ilgner <c.ilgner@tmg-web.de>
2022-08-25 11:57:45 +0200

    ADD: More Objects Types to RowBuilder.with()

--- src/main/java/de/goldendeveloper/mysql/entities/RowBuilder.java
+++ src/main/java/de/goldendeveloper/mysql/entities/RowBuilder.java
@@ -15,6 +15,21 @@ public class RowBuilder {
         return this;
     }
 
+    public RowBuilder with(Column column, Boolean item) {
+        this.insertMap.put(column.getName(), String.valueOf(item));
+        return this;
+    }
+
+    public RowBuilder with(Column column, Long item) {
+        this.insertMap.put(column.getName(), String.valueOf(item));
+        return this;
+    }
+
+    public RowBuilder with(Column column, Integer item) {
+        this.insertMap.put(column.getName(), String.valueOf(item));
+        return this;
+    }
+
     public HashMap<String, String> build() {
         return insertMap;
     }
